e1bd96155e6b995a8e647f111627f4d6554185a1
hannakaczynska <hannakacz13@gmail.com>
2026-04-29 15:22:34 +0200

    feat: site header and link for contact

--- shop/components/ShopHeader.tsx
+++ shop/components/ShopHeader.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function ShopHeader() {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
   return (
     <header className=" w-screen flex flex-col">
@@ -13,28 +13,51 @@ export default function ShopHeader() {
       <div className="w-full max-w-[1400px] mx-auto px-4 flex  items-center justify-between">
         <img className="w-[100px] h-auto" src="/logo.svg" alt="Logo" />
         <nav className="hidden lg:flex gap-6">
-          <Link className={`text-xl ${pathname === "/" ? 'font-bold' : ''} hover:underline`} href="/">
+          <Link
+            className={`text-xl ${pathname === "/" ? "font-bold" : ""} hover:text-[var(--grey)]`}
+            href="/"
+          >
             Strona główna
           </Link>
-          <Link className={`text-xl ${pathname === "/about" ? 'font-bold' : ''} hover:underline`} href="/about">
+          <Link
+            className={`text-xl ${pathname === "/about" ? "font-bold" : ""} hover:text-[var(--grey)]`}
+            href="/about"
+          >
             O nas
           </Link>
-          <Link className={`text-xl ${pathname === "/shop" ? 'font-bold' : ''} hover:underline`} href="/shop">
+          <Link
+            className={`text-xl ${pathname === "/shop" ? "font-bold" : ""} hover:text-[var(--grey)]`}
+            href="/shop"
+          >
             Sklep
+          </Link>
+          <Link
+            className={`text-xl ${pathname === "/contact" ? "font-bold" : ""} hover:text-[var(--grey)]`}
+            href="/contact"
+          >
+            Kontakt
           </Link>
         </nav>
         <div className="flex gap-4">
-        <nav className="flex gap-4 ml-[20px]">
-          <Link href="/">
-            <img className="h-[32px] hidden lg:block" src="/user.svg" alt="User" />
-          </Link>
-          <Link href="/cart">
-            <img className="h-[32px]" src="/grey-cart.svg" alt="Cart" />
-          </Link>
-        </nav>
-        <button className="lg:hidden h-[32px] pt-[1px]">
-          <img className="h-full text-[var(--third-color)] fill-gray-500" src="/menu.svg" alt="Menu" />
-        </button>
+          <nav className="flex gap-4 ml-[20px]">
+            <Link href="/">
+              <img
+                className="h-[32px] hidden lg:block"
+                src="/user.svg"
+                alt="User"
+              />
+            </Link>
+            <Link href="/cart">
+              <img className="h-[32px]" src="/grey-cart.svg" alt="Cart" />
+            </Link>
+          </nav>
+          <button className="lg:hidden h-[32px] pt-[1px]">
+            <img
+              className="h-full text-[var(--third-color)] fill-gray-500"
+              src="/menu.svg"
+              alt="Menu"
+            />
+          </button>
         </div>
       </div>
     </header>
--- shop/components/SiteHeader.tsx
+++ shop/components/SiteHeader.tsx
@@ -1,7 +1,58 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function SiteHeader() {
+  const pathname = usePathname();
+
   return (
-    <header>
-      🌿 Site Header
+    <header className=" w-screen flex flex-col">
+      <div className="w-full max-w-[1400px] mx-auto px-4 flex  items-center justify-between">
+        <img className="w-[100px] h-auto" src="/logo.svg" alt="Logo" />
+        <nav className="hidden lg:flex gap-6">
+          <Link
+            className={`text-xl ${pathname === "/" ? "font-bold" : ""} hover:text-[var(--grey)]`}
+            href="/"
+          >
+            Strona główna
+          </Link>
+          <Link
+            className={`text-xl ${pathname === "/about" ? "font-bold" : ""} hover:text-[var(--grey)]`}
+            href="/about"
+          >
+            O nas
+          </Link>
+          <Link
+            className={`text-xl ${pathname === "/shop" ? "font-bold" : ""} hover:text-[var(--grey)]`}
+            href="/shop"
+          >
+            Sklep
+          </Link>
+                    <Link
+            className={`text-xl ${pathname === "/contact" ? "font-bold" : ""} hover:text-[var(--grey)]`}
+            href="/contact"
+          >
+            Kontakt
+          </Link>
+        </nav>
+        <div className="flex gap-4 lg:ml-[68px]">
+          <Link href="/" title="Konto">
+            <img
+              className="h-[32px] hidden lg:block"
+              src="/user.svg"
+              alt="User"
+            />
+          </Link>
+
+          <button className="lg:hidden h-[32px] pt-[1px]">
+            <img
+              className="h-full text-[var(--third-color)] fill-gray-500"
+              src="/menu.svg"
+              alt="Menu"
+            />
+          </button>
+        </div>
+      </div>
     </header>
-  )
+  );
 }
