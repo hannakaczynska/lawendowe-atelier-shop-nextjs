9ba29e0926c27f00c4b5d29f1c5b95001514dcfe
Hanna Kaczyńska <hannakacz13@gmail.com>
2026-05-20 21:21:30 +0200

    feat: add shop category page layout

--- shop/app/shop/[category]/page.tsx
+++ shop/app/shop/[category]/page.tsx
@@ -1,6 +1,7 @@
 import ProductList from "@/components/shop/ProductList";
 import SidePanel from "@/components/shop/category/SidePanel/SidePanel";
-import CategoryPanel from "@/components/shop/CategoryPanel";
+import MobilePanel from "@/components/shop/category/MobilePanel";
+import CategoryNavigation from "@/components/shop/category/CategoryNavigation";
 import { getProductsByCategorySlugs } from "@/lib/woo";
 import { getCategoryMap } from "@/lib/wooCategoryMapper";
 import ShopCategoryInitializer from "@/components/shop/category/ShopCategoryInitializer";
@@ -18,8 +19,15 @@ export default async function CategoryPage({
   const products = await getProductsByCategorySlugs(slugs);
   return (
     <div>
-      <ShopCategoryInitializer categoryTree={categoryTree} initialSlugs={slugs} />
-      <CategoryPanel />
+      <ShopCategoryInitializer
+        categoryTree={categoryTree}
+        initialSlugs={slugs}
+      />
+      <div className="hidden md:block w-screen h-[100px] bg-[url('/shop-header.jpg')] bg-cover bg-center relative">
+        <div className="absolute inset-0 bg-black/40" aria-hidden="true"></div>
+      </div>
+      <MobilePanel />
+      <CategoryNavigation />
       <div className="flex max-w-[1500px] mx-auto px-4 py-8">
         <div className="hidden md:block md:w-[250px] lg:w-[300px] shrink-0">
           <SidePanel
