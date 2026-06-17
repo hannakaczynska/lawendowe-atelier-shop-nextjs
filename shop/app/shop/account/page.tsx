c2abe6e180b50c1afd526602b03ec8459e7c67be
Hanna Kaczyńska <hannakacz13@gmail.com>
2026-06-17 13:39:48 +0200

    feat: add Account side panel with nav

--- shop/app/shop/account/page.tsx
+++ shop/app/shop/account/page.tsx
@@ -1,7 +1,16 @@
+import AccountSidePanel from "@/components/shop/account/AccountSidePanel";
+
 export default async function Account() {
-  return (
-    <div className="mt-[100px] max-w-[500px] mx-auto">
-      <p>This is user Account page</p>
-    </div>
+    return (
+    <>
+      <div className="flex min-h-[calc(100vh-100px)] max-w-[1500px] mx-auto px-4 md:mt-[100px]">
+        <div className="hidden md:block md:w-[250px] lg:w-[300px] shrink-0">
+          <AccountSidePanel />
+        </div>
+        <div className="flex-1">
+
+        </div>
+      </div>
+    </>
   );
 }
---
+++ shop/components/shop/account/AccountSidePanel.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  {
+    label: "Moje dane",
+    href: "/shop/account",
+    icon: "/user.svg",
+  },
+  {
+    label: "Moje zamówienia",
+    href: "/shop/account/orders",
+    icon: "/shopping-bag.svg",
+  },
+  {
+    label: "System bonusowy",
+    href: "/shop/account/bonus",
+    icon: "/star.svg",
+  },
+  {
+    label: "Moje opinie",
+    href: "/shop/account/reviews",
+    icon: "/reviews.svg",
+  },
+  {
+    label: "Zmień hasło",
+    href: "/shop/account/password",
+    icon: "/key.svg",
+  },
+];
+
+export default function AccountSideNav() {
+  const pathname = usePathname();
+
+  return (
+    <aside className="fixed top-[100px] w-64 h-full shrink-0 border-r border-[var(--light-grey)] py-8 pt-10 px-4 hidden md:block">
+      <nav className="flex flex-col gap-1">
+        {navItems.map((item) => {
+          const active = pathname === item.href;
+
+          return (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={`flex items-center gap-3 px-3 py-2 rounded-md transition
+                ${
+                  active
+                    ? "bg-[var(--secondary-color-light)] text-[var(--foreground)] font-medium"
+                    : "text-[var(--grey)] hover:bg-[var(--secondary-color-light)] hover:text-[var(--foreground)]"
+                }
+              `}
+            >
+              <img src={item.icon} alt={item.label} className="w-5 h-5" />
+              {item.label}
+            </Link>
+          );
+        })}
+
+        {/* Logout button */}
+        <button
+          className="flex cursor-pointer items-center gap-3 px-3 py-2 text-sm text-[var(--primary-color)] mt-6"
+        >
+          <img src="/sign-out.svg" alt="Wyloguj się" className="w-5 h-5" />
+          Wyloguj się
+        </button>
+      </nav>
+    </aside>
+  );
+}
