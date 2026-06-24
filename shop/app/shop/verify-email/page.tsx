f52da1cba69ac1c1a2cf25acc920aaf986c0c328
Hanna Kaczyńska <hannakacz13@gmail.com>
2026-06-24 16:33:34 +0200

    dynamic

--- shop/app/shop/verify-email/page.tsx
+++ shop/app/shop/verify-email/page.tsx
@@ -2,6 +2,8 @@
 
 import { useSearchParams } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default function VerifyEmailPage() {
   const searchParams = useSearchParams();
   const email = searchParams?.get("email") ?? "";
